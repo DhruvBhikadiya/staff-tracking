9da35548202a55a8a26c6f91b1588c7c2b47a999
Dhruv Bhikadiya <138368408+DhruvBhikadiya@users.noreply.github.com>
2024-11-28 11:08:21 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const routes = express.Router();
-const { registration, login, forgotPassword, verifyotp, changePass, getAllUsers, thumbIn, thumbOut, addOrders, addPayment, getOrders, getPayments, getThumbinData, getThumboutData, trackLocatoin, travellingTimeline, getUserInfo } = require('../controller/user.js');
+const {
+  registration,
+  login,
+  forgotPassword,
+  verifyotp,
+  changePass,
+  thumbIn,
+  thumbOut,
+  addOrders,
+  addPayment,
+  trackLocatoin,
+  getUserInfo
+} = require('../controller/user.js');
 
 const thumbIns = require('../model/thumbIns.js');
 const thumbOuts = require('../model/thumbOuts.js');
@@ -9,7 +21,7 @@ const payments = require('../model/payment.js');
 
 const { jwtAuthMiddleware } = require('../config/JWTtoken.js');
 
-routes.get('/', async (req,res) => {
+routes.get('/', async (req, res) => {
   res.send("Hello from server");
 });
 
@@ -26,33 +38,19 @@ routes.post('/verifyotp/:email', verifyotp);
 
 routes.put('/changePass/:email', changePass);
 
-// GET ALL USERS
-routes.get('/getAllUsers', jwtAuthMiddleware, getAllUsers);
-
 // THUMB IN AND THUMB OUT
 routes.post('/thumbin', jwtAuthMiddleware, thumbIns.uploadimage, thumbIn);
 
-routes.get('/getThumbIn', jwtAuthMiddleware, getThumbinData);
-
 routes.post('/thumbout', jwtAuthMiddleware, thumbOuts.uploadimage, thumbOut);
-
-routes.get('/getThumbOut', jwtAuthMiddleware, getThumboutData);
 
 // TRACK LOCATION
 routes.post('/trackLocation', jwtAuthMiddleware, trackLocatoin);
 
 // ADD ORDERS
-routes.post('/addOrder', orders.uploadimage, addOrders);
-
-routes.get('/getOrders', jwtAuthMiddleware, getOrders);
+routes.post('/addOrder', jwtAuthMiddleware, orders.uploadimage, addOrders);
 
 // ADD PAYMENTS
-routes.post('/addPayment', payments.uploadimage, addPayment);
-
-routes.get('/getPaymentsData', jwtAuthMiddleware, getPayments);
-
-// MAP VIEW TO CHECK TRAVELLING TIMELINE
-routes.get('/mapView', jwtAuthMiddleware, travellingTimeline);
+routes.post('/addPayment', jwtAuthMiddleware, payments.uploadimage, addPayment);
 
 // TODAY THUMBIN DATA
 routes.get('/getUserinfo', jwtAuthMiddleware, getUserInfo);
