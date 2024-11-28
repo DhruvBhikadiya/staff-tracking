91a08f53ecec9748128b205362c28c55fb215fc4
Dhruv Bhikadiya <138368408+DhruvBhikadiya@users.noreply.github.com>
2024-11-28 11:14:58 +0530

    Update location.js

--- model/location.js
+++ model/location.js
@@ -6,11 +6,11 @@ const schema = mongoose.Schema({
         required: true
     },
     lat: {
-        type: String,
+        type: Number,
         required: true
     },
     long:{
-        type :String,
+        type :Number,
         required: true
     }
 },
