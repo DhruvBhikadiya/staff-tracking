d5550ebbdabd02418f9100714199a93a4958c10d
Dhruv Bhikadiya <138368408+DhruvBhikadiya@users.noreply.github.com>
2025-07-19 11:24:19 +0530

    Delete routes directory

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const routes = express.Router();
-
-routes.use("/api/", require("./user.js"));
-routes.use("/api/admin", require("./admin.js"));
-
-module.exports = routes;
